66bc547e30326cdd1142fdb5152af4f3d57dd598
Sergio Rodriguez <sergrodrig@gmail.com>
2023-12-02 21:32:52 -0500

    delete working

--- DAL/Enrollments.cs
+++ DAL/Enrollments.cs
@@ -53,8 +53,8 @@ namespace College1en.DAL
         {
             using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString))
             {
-                string insertQuery = "UPDATE Enrollments SET CId = @newCId WHERE StId = @StId AND CId = @oldCId";
-                SqlCommand command = new SqlCommand(insertQuery, connection);
+                string updateQuery = "UPDATE Enrollments SET CId = @newCId WHERE StId = @StId AND CId = @oldCId";
+                SqlCommand command = new SqlCommand(updateQuery, connection);
                 command.Parameters.Add("@newCId", SqlDbType.VarChar, 7).Value = newEnrollment.CId;
                 command.Parameters.Add("@StId", SqlDbType.VarChar, 10).Value = originalEnrollment.StId;
                 command.Parameters.Add("@oldCId", SqlDbType.VarChar, 7).Value = originalEnrollment.CId;
@@ -67,9 +67,24 @@ namespace College1en.DAL
             adapter.Update(ds.Tables["Enrollments"]);
         }
 
-        internal static void DeleteData(List<Enrollment> enrollmentsToDelete)
+        internal static void DeleteData(List<Enrollment> list)
         {
-
+            foreach (Enrollment enrollment in list)
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString))
+                {
+                    string deleteQuery = "DELETE FROM Enrollments WHERE StId = @StId AND CId = @CId";
+                    SqlCommand command = new SqlCommand(deleteQuery, connection);
+                    command.Parameters.Add("@StId", SqlDbType.VarChar, 10).Value = enrollment.StId;
+                    command.Parameters.Add("@CId", SqlDbType.VarChar, 7).Value = enrollment.CId;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            ds.Clear();
+            adapter.Fill(ds, "Enrollments");
+            adapter.Update(ds.Tables["Enrollments"]);
         }
 
     }
